fb64bf0b4229447cd41a4f418f9518c3319b6ff8
AdamSPi <adamsp123@gmail.com>
2017-01-20 13:24:31 -0500

    'Test'

--- Web.js
+++ Web.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
-
+var fs = require('fs');
 app.get('/', function(request, response,err){
-  response.send('NEPS Knowledge Base!');
+  var html = fs.readFileSync();
+  response.send(html);
+
 }).listen(3000, function(){
   console.log("Listening on port 8080");
 });
